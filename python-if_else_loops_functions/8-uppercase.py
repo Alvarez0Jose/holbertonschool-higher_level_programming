410a55cf7e53e25cb8be9c48a9cac8a6697419da
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-01 20:25:43 -0400

    fixing a typo

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for char in str:
-        if ord(char) >= 97 and ord(chra) <= 122:
+        if ord(char) >= 97 and ord(char) <= 122:
             upper = chr(ord(char) - 32)
         else:
             upper = char
