792948e914cae79cb705123dfe3f01a75686e314
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-06 14:59:45 -0400

    adding task 3

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ "__main__":
+
+    args = sys.argv[1:]
+    args_count = len(args)
+
+    if args_count == 0:
+        print("{} arguments.".format(args_count))
+    elif args_count == 1:
+        print("{} arguments:".format(args_count), end+"\n")
+        print(f"{args_count}:", "".join(args), end="\n")
+    else:
+        print(f"{args_count} arguments:", end="\n")
+        for i, arg in enumerate(args, start=1):
+            print(f"{i}: {arg}", end="\n")
