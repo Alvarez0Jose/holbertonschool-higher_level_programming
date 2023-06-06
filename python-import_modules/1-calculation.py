8f1dc7bb897d2affc3b0585ee12b1f08cd8e1cc6
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-06 14:31:04 -0400

    adding a readme file, and 2 tasks

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+form calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    addition = add(a, b)
+    print("{} + {} = {}".format(a, b, addition), end="\n")
+
+    substraction = sub(a, b)
+    print("{} - {} = {}".format(a, b, substraction), end="\n")
+
+    multiplication = mul(a, b)
+    print("{} * {} = {}".format(a, b, multiplication), end="\n")
+
+    division = div(a, b)
+    print("{} / {} = {}".format(a, b, division), end="\n")
