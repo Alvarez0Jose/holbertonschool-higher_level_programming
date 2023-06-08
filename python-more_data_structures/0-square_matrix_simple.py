07b793083489e0b925df2816336047776999d79a
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-08 00:51:15 -0400

    fixing typos

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,15 +4,17 @@ def square_matrix_simple(matrix=[]):
 
     if matrix is None:
         return
+
     if len(matrix) == 0:
         return
+
     NewMatrix = []
 
     for i in matrix:
         list = []
 
-        for digit in i:
-            list.append(digit ** 2)
+        for dgt in i:
+            list.append(dgt ** 2)
 
         NewMatrix.append(list)
 
--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,6 +2,6 @@
 
 def common_elements(set_1, set_2):
 
-    SameElements = set_1.intersecton(set_2)
+    SameElements = set_1.intersection(set_2)
 
     return SameElements
