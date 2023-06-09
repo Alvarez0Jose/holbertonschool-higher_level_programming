1c64e0033e6371a7542bd946b8f80fc2627bee52
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-08 21:48:56 -0400

    fixing identation problems

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,15 +3,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for item in my_list:
 
-        try:
-            if isinstance(item, int):
-                print("{:d}".format(item), end="")
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
                 count += 1
-        except ValueError:
-            pass
-        if count == x:
-            break
-    print()
+        print()
+    except IndexError:
+        pass
     return count
