87139d4271eff2d5a08e28f2cd9129b0ec5d0603
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-13 12:09:00 -0400

    fixing the comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,12 +3,15 @@
 
     """
     This script creates a class with a private instance of size
+
     """
 
 class Square:
+
 
     def __init__(__size):
 
         """
         Here we defines size as a private attribute
+
         """
