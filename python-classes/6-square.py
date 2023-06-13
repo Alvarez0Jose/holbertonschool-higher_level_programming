3e24ddf2a0179269cfd046f3e5d25b370b627107
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-13 13:59:29 -0400

    adding task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""
+Creating a Class Square
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        creating the first method
+        """
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """
+        Area of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Printing the Square
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+
+    @property
+    def size(self):
+        """
+        retrieves the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets size of the value
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or not all(i >= 0 for i in value):
+            raise TypeError("Position must be a tuple of 2 positive integers")
+        self.__postition = value
