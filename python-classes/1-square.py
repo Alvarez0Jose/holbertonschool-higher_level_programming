89cb967e7964e965446a651180c9ea4e02f314a1
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-13 12:04:30 -0400

    adding task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+    """
+    This script creates a class with a private instance of size
+    """
+
+class Square:
+
+    def __init__(__size):
+
+        """
+        Here we defines size as a private attribute
+        """
