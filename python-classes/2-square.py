68ed2576b16a03cc048f94abbf35b1eee24a8d53
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-13 12:32:43 -0400

    fixing the comments

---
+++ python-classes/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+
+"""
+This script creates a class for square
+
+"""
+
+
+class Square:
+
+    """
+    This class represents the square
+    """
+
+    def __init__(self, size=0):
+        """
+        Here we're creating the first method with size
+
+        Raises:
+        TypeError: if size is not integer.
+        ValueError: if size < 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
