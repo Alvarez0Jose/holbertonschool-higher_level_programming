f692e6cd909c97c8bc951ca276e66b479c7ecf31
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-15 17:18:25 -0400

    adding task 0 and README

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Here we're gonna create an empty rectangle class"""
+
+
+class Rectangle:
+    """Empty Rectangle class"""
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Here we're going to work with more classes.
