e1b5a036ca8c9a259a439d2ece78c668144e35c9
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-15 18:08:23 -0400

    editing comments

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,24 +4,37 @@
 Creating a Rectangle class
 """
 
+
 class Rectangle:
     """Rectangle class we're gonna be working on.
     """
+
     def __init__(self, width=0, height=0):
         """
-        provides de parameters for width and height
+        Initializes a Rectangle.
+        Parameters:
+            width: The width of the Rectangle
+            height: The height of the Rectangle
         """
         self.width = width
         self.height = height
 
     @property
-    """Creating a private Width"""
     def width(self):
+        """Getter method for width
+        Returns: the width of the rectangle
+        """
         return self.__width
 
     @width.setter
-    """Setting the Width parameters"""
     def width(self, value):
+        """Setter method for the width
+        Parameter:
+            value: the width value
+        Raises:
+            TypeError: if the width is not an integer.
+            ValueError: if the width is less than 0.
+            """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,13 +42,21 @@ class Rectangle:
         self.__width = value
 
     @property
-    """making a private height"""
     def height(self):
+        """Getter method for height
+        Returns: the height of the rectangle
+        """
         return self.__height
 
     @height.setter
-    """Setting the Height parameters"""
     def height(self, value):
+        """Setter method for the height.
+        parameter: value: the height value.
+
+        Raises:
+            TypeError: if the height is not an integer.
+            ValueError: if the height is less than 0
+            """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
