d02a2ba0c04eb454a2a3a091e95af3473a3e8cba
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-26 11:50:26 -0400

    fixing task

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ Module creates a Rectangle class that inherets form Base
 """
 
 from models.base import Base
+import sys
 
 
 class Rectangle(Base):
@@ -18,28 +19,63 @@ class Rectangle(Base):
             width
             height
         """
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
+        super().__init__(id)
         self.width = width
-
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
         self.height = height
-
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
         self.x = x
-
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
         self.y = y
 
-        super().__init__(id)
+        @property
+        def width(self):
+            """width property"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """Width setter"""
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be > 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """height property"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """height setter"""
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height must be > 0")
+            self.__height = value
+
+        @property
+        def x(self):
+            """x property"""
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            """x setter"""
+            if not isinstance(value, int):
+                raise TypeError("x must be an integer")
+            if value < 0:
+                raise ValueError(" x must be >= 0")
+            self.__x = value
+
+        @property
+        def y(self):
+            """y property"""
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            if not isinstance(value, int):
+                raise TypeError("y must be an integer")
+            if value < 0:
+                raise ValueError("y must be >= 0")
+            self.__y = value
