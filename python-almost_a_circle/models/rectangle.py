4a8e60253acc8dbf2562be6ce646a16860448d0d
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-26 11:17:36 -0400

    adding task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,8 +12,30 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
+        """
+        Method for creating a rectangle
+        Parameters:
+            width
+            height
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
         self.width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
         self.height = height
+
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.x = x
+
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.y = y
+
+        super().__init__(id)
