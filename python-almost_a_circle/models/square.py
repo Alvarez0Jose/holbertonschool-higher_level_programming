e6bfab0ba9cdb2e15d4a9658fb0f082e82685481
Alvarez0Jose <josealejandro220@gmail.com>
2023-06-29 16:30:48 -0400

    adding to the square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Module for a Square class to inheret from Rectangle
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes intances
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Method for Strs
+        """
+        str_square = "[Square] "
+        str_id = f"({self.id}) "
+        str_xy = f"{self.x}/{self.y} - "
+        str_hw = f"{self.width}"
+        str_return = str_square + str_id + str_xy + str_hw
+        return str_return
+
+    @property
+    def size(self):
+        """
+        size getter
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Size setter
+        """
+        self.width = value
+        self.height = value
