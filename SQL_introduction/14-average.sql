be824cad6a0da1756e75c511d971b5a6e5366af1
Alvarez0Jose <josealejandro220@gmail.com>
2023-07-26 18:56:17 -0400

    fixing tasks 9, 14 and 16

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,3 @@
 -- Computing the avereage score of all records in second_table
 
-SELECT AVG(score) AS avereage
-FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,6 +1,3 @@
 -- LISTING all records of second_table
 
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL AND name 1= ''
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name ORDER BY score DESC;
