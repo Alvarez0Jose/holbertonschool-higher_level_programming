638de9af324fbf724a3b63fa506fbc1f1f4d3c37
Alvarez0Jose <josealejandro220@gmail.com>
2023-07-26 20:07:40 -0400

    fixing output

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates the MySQL Server user
-CREATE USER IF NOT EXISTS 'user_od_1' @'localhost' IDENTIFIED BY 'user_od_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_od_1' @'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost';
